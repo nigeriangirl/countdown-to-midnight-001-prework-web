ea27eed66ccadd3b149997c3c8a46ceace9b7685
nigeriangirl <onye@smmaven.com>
2016-01-07 18:06:19 -0500

    Count

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(number)
+  a = number
+  while a > 0
+    puts "#{a} SECOND(S)!"
+    a -= 1
+  end
+ "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  a = number
+  while a > 0
+    puts "#{a} SECOND(S)!"
+    sleep(1)
+    a -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
